e45aabca6ceffd5480d75d425c38d9d178337606
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-02-27 16:01:02 +0000

    completes Parameterizer resource type/parsing
    also, implements Stringer interface on Parameterizer for pretty printing and debugging purposes

--- pkg/parameterizer/types.go
+++ pkg/parameterizer/types.go
@@ -1,6 +1,11 @@
 package parameterizer
 
-// Resource encodes the Parameterizer resource.
+import (
+	"bytes"
+	"fmt"
+)
+
+// Resource represents the `Parameterizer` resource.
 type Resource struct {
 	Kind       string `yaml:"kind"`
 	APIVersion string `yaml:"apiVersion"`
@@ -16,6 +21,7 @@ type Resource struct {
 	} `yaml:"spec"`
 }
 
+// presource represents the `resources` sub-resource.
 type presource struct {
 	Name   string `yaml:"name"`
 	Source struct {
@@ -24,6 +30,7 @@ type presource struct {
 	Volume pvolume `yaml:"volume"`
 }
 
+// puserinput represents the `userInputs` sub-resource.
 type puserinput struct {
 	Name   string `yaml:"name"`
 	Source struct {
@@ -34,6 +41,7 @@ type puserinput struct {
 	Volume pvolume `yaml:"volume"`
 }
 
+// pvolume represents the `volume` sub-resource.
 type pvolume struct {
 	Name     string `yaml:"name"`
 	HostPath struct {
@@ -44,6 +52,41 @@ type pvolume struct {
 	} `yaml:"emptyDir,omitempty"`
 }
 
+// papply represents the `apply` sub-resource.
 type papply struct {
-	Image string `yaml:"image"`
+	Image        string   `yaml:"image"`
+	Commands     []string `yaml:"commands"`
+	VolumeMounts []struct {
+		Name      string `yaml:"name"`
+		mountPath string `yaml:"mountPath"`
+	} `yaml:"volumeMounts"`
+}
+
+func (parameterizer Resource) String() string {
+	var buffer bytes.Buffer
+	buffer.WriteString(fmt.Sprintf("Parameterizer {\n name: %v\n", parameterizer.Metadata.Name))
+	buffer.WriteString(fmt.Sprintf(" resources:\n"))
+	for _, r := range parameterizer.Spec.Resources {
+		buffer.WriteString(fmt.Sprintf("  - %v\n", r.Name))
+		for _, u := range r.Source.URLs {
+			buffer.WriteString(fmt.Sprintf("    - %v\n", u))
+		}
+	}
+	buffer.WriteString(fmt.Sprintf(" user inputs:\n"))
+	for _, u := range parameterizer.Spec.UserInputs {
+		buffer.WriteString(fmt.Sprintf("  - %v\n", u.Name))
+	}
+	buffer.WriteString(fmt.Sprintf(" volumes:\n"))
+	for _, v := range parameterizer.Spec.Volumes {
+		buffer.WriteString(fmt.Sprintf("  - %v\n", v.Name))
+	}
+	buffer.WriteString(fmt.Sprintf(" apply:\n"))
+	for _, a := range parameterizer.Spec.Apply {
+		buffer.WriteString(fmt.Sprintf("  - %v\n", a.Image))
+		for _, c := range a.Commands {
+			buffer.WriteString(fmt.Sprintf("    - %v\n", c))
+		}
+	}
+	buffer.WriteString(fmt.Sprintf("}\n"))
+	return buffer.String()
 }
